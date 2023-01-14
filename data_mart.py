5c3ee5077d16e2b86680d0925d7213fd9f2656fe
artemmint <artembaobab@gmail.com>
2023-01-14 20:47:11 +0300

    initial

---
+++ data_mart.py
@@ -0,0 +1,14 @@
+
+class DataMart:
+    def __init__(self):
+        """Reads data from data source, proceeds it and makes test, train split"""
+        pass
+
+    def get_train_data(self):
+        pass
+
+    def get_test_data(self):
+        pass
+
+
+
---
+++ naive_bayes.py
@@ -0,0 +1,13 @@
+
+
+
+class NaiveBayes:
+    def __init__(self):
+        pass
+
+    def train(self):
+        pass
+
+    def save_model(self):
+        pass
+
